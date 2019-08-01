260196d505ace7261c443cf42dd2e987fb153c8d
Peter Byfield <byfield554@gmail.com>
2019-08-01 21:21:51 +0100

    Hammer config

--- web-ui/src/app/app.module.ts
+++ web-ui/src/app/app.module.ts
@@ -1,13 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
+import { HammerConfig } from './hammer-gesture-config';
 
 @NgModule({
   declarations: [AppComponent, GameComponent],
   imports: [BrowserModule],
-  providers: [],
+  providers: [
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: HammerConfig
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
